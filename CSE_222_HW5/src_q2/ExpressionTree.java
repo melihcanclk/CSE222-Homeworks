76b9a61f9d8514be5329d50a0e35688fe64ecc83
melihcanclk <melihcanclk@github.com>
2020-04-19 02:12:28 +0300

    feat(CSE_222_HW5_Q2) : postOrder done

--- CSE_222_HW5/src_q2/ExpressionTree.java
+++ CSE_222_HW5/src_q2/ExpressionTree.java
@@ -3,34 +3,23 @@ import java.util.Scanner;
 public class ExpressionTree extends BinaryTree<String>{
 
     ExpressionTree(String string){
-        root = readBinaryTree(new Scanner(string));
+        root = readBinaryTree(new Scanner(string)).root;
     }
     ExpressionTree(Node<String> node){
         super(node);
     }
 
-    public static Node<String> readBinaryTree(Scanner scan){
+    public static BinaryTree<String> readBinaryTree(Scanner scan){
+        //Determine whether input that is coming is postfix or prefix
+        //according to that, input will be fixed.
+        scan = determineScanner(scan);
+
         String data = scan.next().trim();
-        StringBuilder input = new StringBuilder(data + " ");
-        while(scan.hasNext()){
-            input.append(scan.next()).append(" ");
-        }
-        if(isSymbol(data)){
-            //Prefix
-        }else {
-            //postfix
-            input = new StringBuilder(reverseString(input.toString()));
-            //to understand that loop is over
-        }
-        //to understand that loop is over
-        input.append(" .");
-        scan = new Scanner(input.toString());
-        data = scan.next().trim();
         ExpressionTree expressionTree = new ExpressionTree(new Node<>(data));
         Node<String> currentNode = expressionTree.root;
         data = scan.next().trim();
         //eğer static yapmak zorunlu değil ise burayı kaldır
-        while(!data.equals(".")){
+        while(scan.hasNext()){
             Node<String> tempNode = new Node<>(data);
             if(currentNode.left != null && currentNode.right != null){
                 if(isSymbol(currentNode.left.data)){
@@ -52,7 +41,46 @@ public class ExpressionTree extends BinaryTree<String>{
                 data = scan.next().trim();
             }
         }
-        return expressionTree.root;
+        return expressionTree;
+    }
+    public String postOrderTraverse(){
+        StringBuilder sb = new StringBuilder();
+        postOrderTraverse(root,1,sb);
+        return sb.toString();
+    }
+
+    private void postOrderTraverse(Node<String> node, int depth, StringBuilder sb) {
+        if(node == null){
+            for(int i = 1; i<depth;++i)
+                sb.append("  ");
+            sb.append("null\n");
+        }
+        else{
+            postOrderTraverse(node.left,depth+1,sb);
+            postOrderTraverse(node.right,depth+1,sb);
+            for(int i = 1; i<depth;++i)
+                sb.append("  ");
+            sb.append(node.toString()).append("\n");
+        }
+    }
+
+    private static Scanner determineScanner(Scanner scan){
+        String data = scan.next().trim();
+        StringBuilder input = new StringBuilder(data + " ");
+        while(scan.hasNext()){
+            input.append(scan.next()).append(" ");
+        }
+        if(isSymbol(data)){
+            //Prefix
+        }else {
+            //postfix
+            input = new StringBuilder(reverseString(input.toString()));
+            //to understand that loop is over
+        }
+        //to understand that loop is over
+        input.append(" .");
+        return new Scanner(input.toString());
+
     }
 
     private static boolean isSymbol(String character){
--- CSE_222_HW5/src_q2/Main.java
+++ CSE_222_HW5/src_q2/Main.java
@@ -1,15 +1,15 @@
-import java.io.IOException;
-import java.util.Scanner;
-
 public class Main {
     private static String nameofFile = "output.txt";
     public static void main(String[] args){
 
         //Create a tree for preorder expression
-        ExpressionTree expTree = new ExpressionTree("a b + e f * g * -");
+        ExpressionTree expTree = new ExpressionTree("+ + 10 * 5 15 20");
+        //System.out.println(expTree);
         //Create a tree for postorder expression
         ExpressionTree expTree2 = new ExpressionTree("10 5 15 * + 20 +");
-        System.out.println(expTree);
-        System.out.println(expTree2);
+        System.out.println(expTree.postOrderTraverse());
+        //System.out.println(expTree.preOrderTraverse());
+
+        //System.out.println(expTree2);
     }
 }
