cf3596e11bbcb6128592559096f527ceec5f6973
melihcanclk <melihcanclk@hotmail.com>
2020-06-02 02:38:58 +0300

    feat(CSE_222_HW7) : Q2 implementation for add, remove and find done

--- CSE_222_HW7/src_q2/Main.java
+++ CSE_222_HW7/src_q2/Main.java
@@ -10,6 +10,22 @@ public class Main {
         skipList.add(23);
         skipList.add(44);
         skipList.add(21);
+        skipList.add(56);
+        skipList.add(489);
+        skipList.add(18);
+        skipList.add(61);
+        skipList.add(26);
+        skipList.add(47);
+        skipList.add(23);
+        System.out.println(skipList);
+        System.out.println(skipList.find(44));
+        skipList.remove(8);
+        skipList.remove(23);
+        skipList.remove(43);
+        skipList.remove(21);
+        skipList.remove(23);
+        skipList.remove(47);
+        skipList.remove(56);
         System.out.println(skipList);
     }
 }
--- CSE_222_HW7/src_q2/SkipList.java
+++ CSE_222_HW7/src_q2/SkipList.java
@@ -1,5 +1,4 @@
-import java.util.Arrays;
-import java.util.Random;
+import java.util.*;
 
 /**
  * Implementation of a Skip-List data structure
@@ -17,34 +16,20 @@ public class SkipList<E extends Comparable<E>> {
 	 */
 	private int size;
 	/**
-	 * The maximum level of the skip-list
-	 */
-	private int maxLevel;
-	/**
-	 * Smallest power of 2 that is greater than the current skip-list size
-	 */
-	private int maxCap;
-	/**
 	 * Natural log of 2
 	 */
 	static final double LOG2 = Math.log(2.0);
 	/**
 	 * Minimum possible integer value for the head
 	 */
-	static final int MIN = Integer.MIN_VALUE;
-	/**
-	 * Random number generator
-	 */
-	private Random rand = new Random();
+	static final Integer MIN = Integer.MIN_VALUE;
 
 	//Constructor
 
 	@SuppressWarnings({ "unchecked", "rawtypes" })
 	public SkipList(){
-		size = 0;
-		maxLevel = 0;
-		maxCap = computeMaxCap(maxLevel);
-		head = new GeneralNode<>(new SLNode(maxLevel, MIN, 0),null);
+		size = 1;
+		head = new GeneralNode<>(null, new SLNode(),null );
 	}
 
 	/**
@@ -53,17 +38,13 @@ public class SkipList<E extends Comparable<E>> {
 	 * @return An SLNode array which references the predecessors of the target at each level.
 	 */
 	@SuppressWarnings("unchecked")
-	private SLNode<E>[] search(E target){
-		SLNode<E>[] pred = (SLNode<E>[]) new SLNode[maxLevel];
-		SLNode<E> current = head.node;
-		for(int i = current.links.length - 1; i >= 0; i--){
-			while(current.links[i] != null
-					&& current.links[i].data[0].compareTo(target) < 0){
-				current = current.links[i];
-			}
-			pred[i] = current;
+	private GeneralNode<E> search(E target){
+
+		GeneralNode<E> current = head;
+		while(current.nextNode != null && target.compareTo(current.node.data.get(current.node.data.size() - 1)) > 0){
+			current = current.nextNode;
 		}
-		return pred;
+		return current;
 	}
 
 	/**
@@ -73,13 +54,14 @@ public class SkipList<E extends Comparable<E>> {
 	 * 		   the target. If not found, null is returned
 	 */
 	public E find(E target){
-		SLNode<E>[] pred = search(target);
-		if(pred[0].links != null &&
-				pred[0].links[0].data[0].compareTo(target) == 0){
-			return pred[0].links[0].data[0];
-		} else {
-			return null;
+		GeneralNode<E> pred = search(target);
+		for(int i = 0; i< pred.node.data.size();i++) {
+			if (pred.node.data.get(i) != null &&
+					pred.node.data.get(i) .compareTo(target) == 0) {
+				return pred.node.data.get(i) ;
+			}
 		}
+		return null;
 	}
 
 	/**
@@ -87,77 +69,57 @@ public class SkipList<E extends Comparable<E>> {
 	 * @param item The item to add
 	 * @return true as the item is added
 	 */
-	boolean add(E item){
-		size++;
-		SLNode<E>[] pred = search(item);
-		if(size > maxCap){
-			maxLevel++;
-			maxCap = computeMaxCap(maxLevel);
-			head.node.links = Arrays.copyOf(head.node.links, maxLevel);
-			pred = Arrays.copyOf(pred, maxLevel);
-			pred[maxLevel - 1] = head.node;
-		}
-		if(pred[0].cursor == SLNode.SIZE_OF_ARRAYS){
-			SLNode<E> newNode = new SLNode<E>(logRandom(), item,0);
-			for(int i = 0; i < newNode.links.length; i++){
-				newNode.links[i] = pred[i].links[i];
-				pred[i].links[i] = newNode;
+	boolean add(E item) {
+		GeneralNode<E> pred = search(item);
+		int index = pred.node.data.indexOf(item);
+		if (index == -1) {
+			pred.node.data.add(item);
+			Collections.sort(pred.node.data);
+			while(pred.node.data.size() == SLNode.SIZE_OF_ARRAYS + 1){
+				if(pred.nextNode == null){
+					pred.nextNode = new GeneralNode<>(pred,new SLNode<>(pred.node.data.get(pred.node.data.size() - 1)),null);
+					pred.node.data.remove(pred.nextNode.node.data.get(0));
+				}else{
+					pred.nextNode.node.data.add(pred.node.data.get(pred.node.data.size() - 1));
+					pred.node.data.remove(pred.node.data.size() -1);
+					Collections.sort(pred.nextNode.node.data);
+				}
+				pred = pred.nextNode;
 			}
-			GeneralNode<E> current = head;
-			while(current.node != pred[0] && current.nextNode != null){
-				current = current.nextNode;
-			}
-			current.nextNode = new GeneralNode<>(newNode, null);
-
 		}else{
-			pred[0].data[pred[0].cursor] = item;
-			pred[0].cursor++;
+			return false;
 		}
 		return true;
 	}
-
 	/**
 	 * Removes an instance of the given item
 	 * @param item The item to remove
 	 * @return true if the item is removed, false if the item is not in the list
 	 */
 	boolean remove(E item){
-		SLNode<E>[] pred = search(item);
-		if(pred[0].links != null &&
-				pred[0].links[0].data[0].compareTo(item) != 0){
-			return false; //item is not in the list
-		} else {
-			size--; //don't re-adjust maxCap and level, as we may have nodes at these levels
-			SLNode<E> deleteNode = pred[0];
-			for(int i = 0; i < deleteNode.links.length; i++){
-				if(pred[i].links[i] != null)
-					pred[i].links[i] = pred[i].links[i].links[i];
+		GeneralNode<E> pred = search(item);
+		int index = pred.node.data.indexOf(item);
+		if(index != -1){
+			pred.node.data.remove(index);
+			if(pred.beforeNode != null){
+				if(pred.beforeNode.node.data.size() + pred.node.data.size() == SLNode.SIZE_OF_ARRAYS){
+					pred.beforeNode.node.data.addAll(pred.node.data);
+					pred.beforeNode.nextNode = pred.nextNode;
+					pred.nextNode.beforeNode = pred.beforeNode;
+					pred = null;
+				}
+			}else if(pred.nextNode != null){
+				if(pred.nextNode.node.data.size() + pred.node.data.size() == SLNode.SIZE_OF_ARRAYS){
+					pred.nextNode.node.data.addAll(pred.node.data);
+					pred.beforeNode.nextNode = pred.nextNode;
+					pred.nextNode.beforeNode = pred.beforeNode;
+					pred = null;
+				}
 			}
-			return true;
+		}else{
+			return false;
 		}
-	}
-
-
-	/**
-	 * Method to generate a logarithmic distributed integer between 1 and maxLevel.
-	 *  I.E. 1/2 of the values are 1, 1/4 are 2, etc.
-	 * @return a random logarithmic distributed int between 1 and maxLevel
-	 */
-	private int logRandom(){
-		int r = rand.nextInt(maxCap);
-		int k = (int) (Math.log(r + 1) / LOG2);
-		if(k > maxLevel - 1)
-			k = maxLevel - 1;
-		return maxLevel - k;
-	}
-
-	/**
-	 * Recompute the max cap
-	 * @param level
-	 * @return
-	 */
-	private int computeMaxCap(int level){
-		return (int) Math.pow(2, level) - 1;
+		return true;
 	}
 
 	@SuppressWarnings("rawtypes")
@@ -166,7 +128,7 @@ public class SkipList<E extends Comparable<E>> {
 			return "Empty";
 		StringBuilder sc = new StringBuilder();
 		GeneralNode itr = head;
-		sc.append("Head: " + maxLevel);
+		sc.append("Head: " + size);
 		int lineMaker = 0;
 		while(itr != null){
 			sc.append(" --> " + itr.node.toString());
@@ -188,34 +150,38 @@ public class SkipList<E extends Comparable<E>> {
 	 */
 	static class SLNode<E>{
 		private static final int SIZE_OF_ARRAYS = 5;
-		SLNode<E>[] links;
-		E []data;
+
+		List<E> data;
 		int cursor;
 		/**
 		 * Create a node of level m
-		 * @param m The level of the node
 		 * @param data The data to be stored
 		 */
 		@SuppressWarnings("unchecked")
-		public SLNode(int m, E data,int index){
-			links = (SLNode<E>[]) new SLNode[m];
-			this.data = (E[])new Comparable[SIZE_OF_ARRAYS];
-			this.data[index] = data;
-			cursor = 1;
+		public SLNode(E data){
+			this.data = new ArrayList<>();
+			this.data.add(data);
+			cursor = 0;
+		}
+		public SLNode(){
+			this.data = new ArrayList<>();
+			cursor = 0;
 		}
 
 
 		public String toString(){
-			return (Arrays.toString(data) + " |" + links.length + "|");
+			return (data + " |" +  "|");
 		}
 	}
 	static class GeneralNode<E>{
 		SLNode<E> node;
 		GeneralNode<E> nextNode;
+		GeneralNode<E> beforeNode;
 
-		public GeneralNode(SLNode<E> node, GeneralNode<E> nextNode) {
+		public GeneralNode( GeneralNode<E> beforeNode,SLNode<E> node, GeneralNode<E> nextNode) {
 			this.node = node;
 			this.nextNode = nextNode;
+			this.beforeNode = beforeNode;
 		}
 	}
 }
